8dd5287e3b1320599fa18c27dbfd54f3b2ea0a43
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-04-24 15:57:57 -0300

    [UP] repo-name

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@ module.exports = {
   siteMetadata: {
     title: `Delog`,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    siteUrl: `https://saulofilho-portfolio.netlify.app/`,
     home: {
-      title: `Hi! I'm Delog`,
+      title: `Hi! I'm Saulo Filho`,
       description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */

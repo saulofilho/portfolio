31ddf4b775f6c01f7a982f0dfa2af77e9b025040
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-11-27 10:29:20 -0300

    [feat] 404 page

---
+++ src/components/Sketch.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Sketch from "react-p5";
+
+export default class SketchTorus extends Component {
+
+  setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    p5.frameRate(100);
+  };
+
+  draw = p5 => {
+    let img = p5.createImage(p5.windowWidth, p5.windowHeight);
+
+    img.loadPixels();
+    p5.background(0);
+
+    function writeColor(image, x, y, red, green, blue, alpha) {
+      let index = (x + y * p5.width) * 4;
+      image.pixels[index] = red;
+      image.pixels[index + 1] = green;
+      image.pixels[index + 2] = blue;
+      image.pixels[index + 3] = alpha;
+    }
+
+    let x, y;
+    for (y = 0; y < img.height; y++) {
+      for (x = 0; x < img.width; x++) {
+        let red = p5.random(255);
+        let green = p5.random(255);
+        let blue = p5.random(255);
+        let alpha = 255;
+        writeColor(img, x, y, red, green, blue, alpha);
+      }
+    }
+
+    img.updatePixels();
+    p5.image(img, 0, 0);
+
+  };
+
+  windowResized = p5 => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+  }
+
+  render() {
+    return <Sketch setup={this.setup} draw={this.draw} windowResized={this.windowResized} />;
+  }
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,16 @@
 import React from "react"
+import Sketch from '../components/Sketch'
+import './404.css'
 
 const page404 = () => {
   return (
-    <div className="page-404">
-      <h1>Page not found.</h1>
-      <h3>Ops! The page you are looking for has been removed or relocated.</h3>
-    </div>
+    <>
+      <div className="page-404">
+        <h1>Page not found.</h1>
+        <h3>Ops! The page you are looking for has been removed or relocated.</h3>
+      </div>
+      <Sketch />
+    </>
   )
 }
 

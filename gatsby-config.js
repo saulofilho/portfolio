0ff3dfbf5f735e9c50672e6c558aef462d29c147
saulo filho <hello@saulofilho.com>
2021-08-23 18:22:48 -0300

    [feat] Software Engineer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Saulo Filho, Full Stack Javascript Developer`,
-    description: `Full Stack Javascript, Ruby, Python and Processing  Developer`,
+    title: `Saulo Filho, Software Engineer`,
+    description: `Javascript, Ruby, Python, Processing && IoT`,
     siteUrl: `https://saulofilho.com`,
     home: {
-      title: `Hi! I'm Saulo Filho`,
-      description: `Olá. :)
-
-      My name is Saulo Filho. I'm a Creative Full Stack Javascript, Ruby, Python and Processing Developer.
-
-      I used to be (10+ years) a Creative Marketing Professional, writing to
-      advertising and content.
-      Cinema, music and entertainment scripts as well.
-
-      Since 2017, I have been working with HTML5, CSS3, Vanilla/ECMA, Node,
-      Processing, and tools like SASS, React, Gatsby, Next, React Native/Expo,
-      Redux, Adonis, Jest, Storybook, P5, Git, Webpack, Gulp, Docker, Regex,
-      JSONt, Markdown, CLI, JAMStack, MERNStack, GraphQL, SQL (MySQL, SQLite, Pstgres),
-      NoSQL (MongoDB), CMS (WordPress, VTEX, Squarespace, Netlify CMS).
-
-      I spend some time having fun with generative art, and making robots,
-      with Arduino and IoT.`,
+      title: `Olá! My name is Saulo Filho. I'm a Software Engineer.`,
+      description: `Javascript, Ruby, Python, Processing && IoT`
     }
   },
   plugins: [
@@ -34,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Saulo Filho, Full Stack Javascript Developer`,
+        name: `Saulo Filho, Software Engineer`,
         short_name: `saulofilho`,
         start_url: `/`,
         background_color: `rgb(127, 246, 27`,
